7af312d5a5fb5335cf555e93349a905f114f73cf
Jiaxuan-Ren <jiaxuan.ren@outlook.com>
2020-02-28 23:00:37 -0800

    Use HTML5 Helios

---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE HTML>
+<!--
+	Helios by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Jiaxuan Ren</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="homepage is-preload">
+		<a href="#" id="scroll" style="display: none;"><span></span></a>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header">
+
+					<!-- Inner -->
+						<div class="inner">
+							<header>
+								<h1><a href="index.html" id="logo">Jiaxuan Ren</a></h1>
+								<hr />
+								<p>Explore my personal website</p>
+							</header>
+							<footer>
+								<a href="#banner" class="button circled scrolly">Start</a>
+							</footer>
+						</div>
+						<?php include("navbar.html"); ?>
+
+				</div>
+
+			<!-- Banner -->
+				<section id="banner">
+					<header>
+						<h2>Hi. You're looking at <strong>Jiaxuan Ren</strong>.</h2>
+						<p>
+							Computer Science student at University of California San Diego. 
+							Interested in economics and social science. 
+						</p>
+					</header>
+				</section>
+
+			<!-- Carousel -->
+				<section class="carousel">
+					<div class="reel">
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Classical Chinese</a></h3>
+							</header>
+							<p>
+								I'm capable of reading <a href="https://en.wikipedia.org/wiki/Classical_Chinese">Classical Chinese</a>, the kind of language used in China two thousand years ago. 
+								I have read <a href="https://en.wikipedia.org/wiki/Records_of_the_Grand_Historian">Records of the Grand Historian</a> by the age of 14, as well as many other literary classics. 
+							</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Fermentum sagittis proin</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Sed quis rhoncus placerat</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Ultrices urna sit lobortis</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<!-- <article>
+							<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Varius magnis sollicitudin</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Pulvinar sagittis congue</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Fermentum sagittis proin</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Sed quis rhoncus placerat</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Ultrices urna sit lobortis</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Varius magnis sollicitudin</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article> -->
+
+					</div>
+				</section>
+
+			<!-- Main -->
+				<div class="wrapper style2">
+
+					<article id="main-ignite" class="container special">
+						<!-- <a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a> -->
+						<div style="text-align:center; padding-top: 50px;">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/4rpDmfqp2QE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>						
+						<header>
+							<h2><a href="#main-ignite">How Tech Companies Avoid Billions of Taxes</a></h2>
+							<p>
+								How much federal taxes did amazon pay in 2018? Why Apple is leaving billions of dollars off-shore? <br />
+								The video is my ignite presentation for CAT125R: Public Rhetoric and Practical Communication in Winter Quarter of 2020 at UCSD. 
+							</p>
+						</header>
+						<!-- <p>
+							The video is my ignite presentation for CAT125R: Public Rhetoric and Practical Communication in Winter Quarter of 2020 at UCSD. 
+						</p> -->
+						<!-- <footer>
+							<a href="#" class="button">Continue Reading</a>
+						</footer> -->
+					</article>
+
+				</div>
+
+				<div class="wrapper style2">
+
+					<article id="main-dmp" class="container special">
+						<!-- <a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a> -->
+						<div style="text-align:center; padding-top: 50px;">
+							<iframe width="560" height="315"  src="https://www.youtube.com/embed/bfP1IMOlt_8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>						
+						<header>
+							<h2><a href="#main-dmp">digital media project</a></h2>
+							<p>
+								How much federal taxes did amazon pay in 2018? Why Apple is leaving billions of dollars off-shore? <br />
+								The video is my ignite presentation for CAT125R: Public Rhetoric and Practical Communication in Winter Quarter of 2020 at UCSD. 
+							</p>
+						</header>
+						<!-- <p>
+							The video is my ignite presentation for CAT125R: Public Rhetoric and Practical Communication in Winter Quarter of 2020 at UCSD. 
+						</p> -->
+						<!-- <footer>
+							<a href="#" class="button">Continue Reading</a>
+						</footer> -->
+					</article>
+
+				</div>
+
+			<!-- Features -->
+				<div class="wrapper style1">
+
+					<section id="features" class="container special">
+						<header>
+							<h2>Morbi ullamcorper et varius leo lacus</h2>
+							<p>Ipsum volutpat consectetur orci metus consequat imperdiet duis integer semper magna.</p>
+						</header>
+						<div class="row">
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
+								<header>
+									<h3><a href="#">Gravida aliquam penatibus</a></h3>
+								</header>
+								<p>
+									Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+									porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+								</p>
+							</article>
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/pic08.jpg" alt="" /></a>
+								<header>
+									<h3><a href="#">Sed quis rhoncus placerat</a></h3>
+								</header>
+								<p>
+									Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+									porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+								</p>
+							</article>
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/pic09.jpg" alt="" /></a>
+								<header>
+									<h3><a href="#">Magna laoreet et aliquam</a></h3>
+								</header>
+								<p>
+									Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+									porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+								</p>
+							</article>
+						</div>
+					</section>
+
+				</div>
+				<?php include("footer.html"); ?>
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ project.php
@@ -0,0 +1,138 @@
+<!DOCTYPE HTML>
+<!--
+	Helios by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Projects - Jiaxuan Ren</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="project is-preload">
+		
+		
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header">
+
+					<!-- Inner -->
+						<div class="inner">
+							<header>
+								<h1><a href="#" id="logo">Projects</a></h1>
+							</header>
+						</div>
+
+						<?php include("navbar.html"); ?>
+						
+
+
+				</div>
+
+			<!-- Main -->
+				<div class="wrapper style1">
+
+					<div class="container">
+						<article id="main" class="special">
+							<header>
+								<h2><a href="#">No Sidebar</a></h2>
+								<p>
+									Morbi convallis lectus malesuada sed fermentum dolore amet
+								</p>
+							</header>
+							<p>
+								Commodo id natoque malesuada sollicitudin elit suscipit. Curae suspendisse mauris posuere accumsan massa
+								posuere lacus convallis tellus interdum. Amet nullam fringilla nibh nulla convallis ut venenatis purus
+								lobortis. Auctor etiam porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum
+								consequat integer interdum integer purus sapien. Nibh eleifend nulla nascetur pharetra commodo mi augue
+								interdum tellus. Ornare cursus augue feugiat sodales velit lorem. Semper elementum ullamcorper lacinia
+								natoque aenean scelerisque vel lacinia mollis quam sodales congue.
+							</p>
+							<section>
+								<header>
+									<a href="https://jiaxuan-ren.github.io/SSH/"><h3>SSH - Student and Student Housing</h3></a>
+								</header>
+								<p>
+									Built an app for UCSD students to find housing and roommates. Our app was selected as best in section
+									Developed multiple pages, including housing details page, roommate details page, etc
+									Languages & Frameworks used: JavaScript, React Native, Firebase
+								</p>
+							</section>
+							<section>
+								<header>
+									<a href="https://jiaxuan-ren.github.io/Trump-tweet-stock-market/"><h3>Trump's Tweet and Stock Market</h3></a>
+								</header>
+								<p>
+									Analyze the short-term impact of Trump’s tweets on the stock market using dataset from NYSE Trade and Quote
+									Build two models, linear regression and LSTM (Long Short Term Memory)
+									Beat popularity predictor by more than 2%, which is a noticable difference for a chaotic system like the stock market
+									Read the Jupyter Notebook [here]
+								</p>
+							</section>
+							<section>
+								<header>
+									<a href="https://www.youtube.com/watch?v=tq0meNEGNZc"><h3>Hong Kong 1998: How George Soros succeeded in Thailand and UK but failed in Hong Kong</h3></a>
+								</header>
+								<p>
+									George Soros, a “financial crocodile”, who have successfully defeated Bank of England and Central Bank of Thailand, declared war on Hong Kong in 1998. 
+									After the handover in 1997, the world is in doubt about whether the Chinese government can successfully deal with Hong Kong. 
+									So what happened in the financial market in 1998, and did Soros failed or not? 
+									If you are interested in these questions, please watch the video to find out.
+								</p>
+							</section>
+							<section>
+								<header>
+									<a href="https://wid.world"><h3>World Inequality Database</h3></a>
+								</header>
+								<p>
+									The World Inequality Database aims to provide open and convenient access to the most extensive available database on the historical evolution of the world distribution of income and wealth, 
+									both within countries and between countries.
+								</p>
+							</section>
+						</article>
+						<hr />
+						<div class="projects-center">
+							<div class="row">
+								<article class="col-4 col-12-mobile special">
+									<a href="#" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
+									<header>
+										<h3><a href="#">Gravida aliquam penatibus</a></h3>
+									</header>
+									<p>
+										Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+										porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+									</p>
+								</article>
+								<article class="col-4 col-12-mobile special">
+									<a href="#" class="image featured"><img src="images/pic08.jpg" alt="" /></a>
+									<header>
+										<h3><a href="#">Sed quis rhoncus placerat</a></h3>
+									</header>
+									<p>
+										Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+										porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+									</p>
+								</article>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<?php include("footer.html"); ?>
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+	</body>
+</html>
